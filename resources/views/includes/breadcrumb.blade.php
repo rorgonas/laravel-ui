f2b2bf692d7f968c056be2746b38f269c9479399
Rolland Orgonas <rolland.orgonas@TMV-DEV-09.local>
2018-04-16 16:36:03 -0400

    laravel-ui: add link back to home page

--- resources/views/includes/breadcrumb.blade.php
+++ resources/views/includes/breadcrumb.blade.php
@@ -1,6 +1,7 @@
 <div class="panel panel-default">
     <div class="panel-body">
         <ul class="breadcrumb">
+          <li><a href="/">Home</a> <span class="divider">/</span></li>
           <li><a href="#">MV Girls</a> <span class="divider">/</span></li>
           <li class="active">List</li>
         </ul>
