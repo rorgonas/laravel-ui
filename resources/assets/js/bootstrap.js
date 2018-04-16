26c6507acbbf73b9bc2b068b02fc03bf3506386f
Rolland Orgonas <rolland.orgonas@TMV-DEV-09.local>
2018-04-16 15:58:52 -0400

    laravel-ui: add vue protection against xss & csrf vulnerabity attacks

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -25,6 +25,11 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
+ * Set Vue XSS and CSRF protection - against vulnerability attacks
+ * */
+window.Laravel = { csrfToken: '{{ csrf_token() }}'};
+
+/**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
  * a simple convenience so we don't have to attach every token manually.
