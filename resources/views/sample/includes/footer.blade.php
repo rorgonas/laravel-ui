15cb2fe22262394b455e1c09849d4a98995be7f8
Rolland Orgonas <rolland.orgonas@TMV-DEV-09.local>
2018-04-20 17:02:50 -0400

    start adding some custom styles

--- resources/views/sample/includes/footer.blade.php
+++ resources/views/sample/includes/footer.blade.php
@@ -1,4 +1,6 @@
 <footer>
+    <h2>This is just a sample page</h2>
+    <p>Use below links for quick navigation through out the pages</p>
     <nav>
         <a href='/about' title='About'>Abut</a>
         <a href='/contact' title='Contact'>Contact</a>
